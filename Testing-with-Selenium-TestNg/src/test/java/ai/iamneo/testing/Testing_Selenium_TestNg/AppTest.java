1c941320b5d001e38e834b85b7b90e1bae632d3c
Iamneo <techadmin@iamneo.ai>
2023-08-10 07:51:13 +0000

    Backup Commit

--- Testing-with-Selenium-TestNg/src/test/java/ai/iamneo/testing/Testing_Selenium_TestNg/AppTest.java
+++ Testing-with-Selenium-TestNg/src/test/java/ai/iamneo/testing/Testing_Selenium_TestNg/AppTest.java
@@ -9,6 +9,11 @@ import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.BeforeTest;
 import org.testng.Assert;
 import org.testng.annotations.AfterTest;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.annotations.Test;
 
 public class AppTest {
 
@@ -16,48 +21,39 @@ public class AppTest {
 	WebDriver driver = null;
 
 	@BeforeTest
-	public void beforeTest() throws Exception
-	 {
+	public void beforeTest() throws Exception {
+		
 		driver = new RemoteWebDriver(new URL("http://localhost:4444"), chromeOptions);
 	}
 
 	@Test
-//Checking the title of iamNeo (Home - iamneo)
-	public void iamNeo() throws InterruptedException 
-	{
-
-		 String title ="Get your title";
-		Assert.assertEquals(title, "");
-	}
-	@Test
-//Moving to FACEBOOK
-	public void nextPage() throws InterruptedException 
-	{
-		 	
-		 String title ="Get your title";
-		Assert.assertEquals(title, "");
-
-	}
-	@Test
-//Back to iamNeo
-	public void backPage() throws InterruptedException 
-	{
-		 String title ="Get your title";
-		Assert.assertEquals(title, "");
-
-	}
-	@Test
-//Current URL
-public void currentURL() throws InterruptedException 
+	public void testcase_1() throws InterruptedException 
 {
-		 String title ="Get your title";
-		Assert.assertEquals(title, "");
+		driver.navigate().to("https://jqueryui.com/droppable/");
+		
+		
+		
+	}
 
-}
+	@Test
+	public void testcase_2() throws InterruptedException 
+      {
+		driver.switchTo().frame(0);
+	    WebElement drag= driver.findElement(By.id("draggable"));
+	    WebElement drop=driver.findElement(By.id("droppable"));
+		Actions a=new Actions(driver);
+	    a.dragAndDrop(drag, drop).build().perform();
+	}
+	@Test
+	public void testcase_3() throws InterruptedException 
+      {
+		WebElement drop=driver.findElement(By.id("droppable"));
+		Assert.assertEquals(drop.getText(),"Dropped!");
+	}
 
+		
 	@AfterTest
-	public void afterTest() 
-	{
+	public void afterTest() {
 		driver.quit();
 	}
 
